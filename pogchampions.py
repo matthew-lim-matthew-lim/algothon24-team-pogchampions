97e7e038f0de54a2a6b04dc61053e0d7aeacee10
mattlsixhundredalt@gmail.com <mattlsixhundredalt@gmail.com>
2024-06-30 22:08:29 +1000

    use a padded simple moving average so that our SMA covers the entire day-range of the original dataset

--- pogchampions.py
+++ pogchampions.py
@@ -10,10 +10,19 @@ nInst = 50
 currentPos = np.zeros(nInst)
 
 
+# Simple Moving Average
+def sma(data, window):
+    # weights array is an array of '1/window'. Multiplying it with the data array will give us the 
+    # moving average since '1/window * data' describes the average of each datapoint
+    weights = np.repeat(1.0, window)/window
+    smas = np.convolve(data, weights, 'valid')
+    return smas
+
 def getMyPosition(prcSoFar):
     global currentPos
     (nins, nt) = prcSoFar.shape
-    if (nt < 2):
+    # If the window is too small, don't make a play (don't have enough data to know)
+    if (nt < 20):
         return np.zeros(nins)
     lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
     lNorm = np.sqrt(lastRet.dot(lastRet))
