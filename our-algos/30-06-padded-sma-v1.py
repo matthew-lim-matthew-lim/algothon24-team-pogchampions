8dfc65096fa387dd6e6193f35dd816f1501deb8b
matthewlimcenterdiv@gmail.com <matthewlimcenterdiv@gmail.com>
2024-06-30 22:51:29 +1000

    added analysis of the padded sma strategy

---
+++ our-algos/30-06-padded-sma-v1.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+
+##### TODO #########################################
+### RENAME THIS FILE TO YOUR TEAM NAME #############
+### IMPLEMENT 'getMyPosition' FUNCTION #############
+### TO RUN, RUN 'eval.py' ##########################
+
+nInst = 50
+currentPos = np.zeros(nInst)
+
+
+# Padded Simple Moving Average
+def sma_padded(data, window):
+    # We pad (window - 1) and (0) (meaning that the SMA extends over the entire
+    # data set)
+    padded_data = np.pad(data, (window-1, 0), mode='edge')
+    weights = np.repeat(1.0, window) / window
+    smas = np.convolve(padded_data, weights, 'valid')
+    return smas
+
+def getMyPosition(prcSoFar):
+    global currentPos
+    (nins, nt) = prcSoFar.shape
+    # If the window is too small, don't make a play (don't have enough data to know).
+    # I mean, technically it works now we use the padded SMA, but it's not a good idea
+    # since the SMA will be very inaccurate with so little data.
+    if (nt < 20):
+        return np.zeros(nins)
+    
+    # The SMA from sma_padded is 1 dimension but a dataframe is 2 dimensions
+    # Also, it needs to be a numpy array (using np.array), otherwise 
+    # the rpos calculation will not work
+    sma_df = np.array([sma_padded(prcSoFar[i], 20) for i in range(nins)])
+
+    # We use the last value of the SMA to determine our position
+    sma_last_pos = sma_df[:, -1]
+
+    # If the actual is SMA is larger than Current Price, we buy
+    rpos = np.array([int(x) for x in 1 * (sma_last_pos - prcSoFar[:, -1])])
+    # currentPos = np.array([int(x) for x in currentPos+rpos])
+    currentPos = np.array([int(x) for x in rpos])
+    print(sma_last_pos[:5])
+    print(prcSoFar[:, -1][:5])
+    print(currentPos[:5])
+    return currentPos
---
+++ our-algos/algo-documentation.md
@@ -0,0 +1,33 @@
+## 28-06-default-algo
+
+```
+mean(PL): -18.2
+return: -0.00095
+StdDev(PL): 276.21
+annSharpe(PL): -1.04 
+totDvolume: 4796462 
+Score: -45.77
+```
+
+## 30-06-padded-sma-v1.py
+- Uses a Padded Simple Moving Average.
+- Beats the default! 
+- Needs tweaking with amount to buy/sell.
+- Definitely a basic algo, but this current implementation can definitely be optimised to perform better.
+
+```
+mean(PL): -0.3
+return: -0.00101
+StdDev(PL): 1.81
+annSharpe(PL): -2.37 
+totDvolume: 67317 
+Score: -0.45
+```
+
+Some ideas:
+- Buy/Sell only when the percentage absolute difference of the SMA and Current Price is greater than a specific threshold (eg. 5%).
+- Different window sizes depending on how volatile a stock is.
+- Different Buy/Sell amounts depending on how volatile a stock is.
+
+Future:
+- Train a model with machine learning and use that
